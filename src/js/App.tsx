ad3034caa32e0e826e8c21236d232dc144a2ca6b
A00303007: Eren Basaran <a00303007@student.tus.ie>
2024-09-27 15:05:19 +0100

    testing the main branch permissions

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          This is a test paragraph to see if I can straight commit to the main branch
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
