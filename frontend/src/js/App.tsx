139357ecad1505c12a16b277a20018b240481c19
A00303007: Eren Basaran <a00303007@student.tus.ie>
2024-10-05 14:21:57 +0100

    Added a separate DesktopLayout to increase the readability in the App.tsx file.

--- frontend/src/js/App.tsx
+++ frontend/src/js/App.tsx
@@ -1,34 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { motion, useScroll, useSpring } from 'framer-motion';
-import {useMediaQuery} from "react-responsive";
-import Charts from './components/Charts';
-import Intro from "./components/Intro";
 import '../css/styles.css';
 import MobileLayout from "./MobileLayout";
-import Header from "./components/Header";
+import DesktopLayout from "./DesktopLayout";
+import {useMediaQuery} from "react-responsive";
 
 export default function App() {
-    const { scrollYProgress } = useScroll();
-
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-        restDelta: 0.001
-    });
 
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
 
     return isTabletOrMobile ?
-        (<MobileLayout/>)
-        :
-        (
-            <>  <Header/>
-                <Intro />
-                {[1, 2, 3, 4, 5, 6, 7, 8].map((id) => (
-                    <Charts key={id} id={id} jsonFileName={"./dashboards.json"} />
-                ))}
-                <motion.div className="progressBar" style={{ scaleX }} />
-            </>
-        );
+        (<MobileLayout/>) :(<DesktopLayout/>);
 
 }
---
+++ frontend/src/js/DesktopLayout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./components/Header";
+import {motion, useScroll, useSpring} from "framer-motion";
+import Intro from "./components/Intro";
+import Charts from "./components/Charts";
+
+const DesktopLayout = () => {
+
+    const { scrollYProgress } = useScroll();
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    });
+
+
+    return (
+        <>
+            <Header/>
+            <Intro />
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((id) => (
+                <Charts key={id} id={id} jsonFileName={"./dashboards.json"} />
+            ))}
+            <motion.div className="progressBar" style={{ scaleX }} />
+        </>
+    )
+}
+
+export default DesktopLayout;
